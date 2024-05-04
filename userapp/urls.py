acb24e212ad0ff488f6317b241945fcc297e0289
bezhan2009 <karimovbezan0@gmail.com>
2024-05-04 23:06:56 +0300

    refactor

--- userapp/urls.py
+++ userapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserProfileView
+from .views import UserProfileView, UserProfileDetails
 
 urlpatterns = [
     path('sign-up/', UserProfileView.as_view(), name='sign-up'),
+    path('details/', UserProfileDetails.as_view(), name='user-details'),
 ]
